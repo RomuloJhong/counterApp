804032e77d3eac74acee39844d718850ff83624a
romulo <dragro92@hotmail.com>
2022-08-14 14:42:59 -0600

    first comment

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import count from "@/components/count.vue";
+</script>
+
+<template>
+  <div>
+    <count />
+  </div>
+</template>
---
+++ src/components/count.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref, watch, onMounted } from "vue";
+
+const count = ref(0);
+
+watch(count, (newVal) => {
+  localStorage.setItem("count", newVal);
+});
+
+onMounted(() => {
+  count.value = localStorage.getItem("count") || "";
+});
+</script>
+
+<template>
+  <h1>Count App</h1>
+  <section>
+    <input
+      type="number"
+      placeholder="amount here"
+      v-model="count"
+      v-if="count >= 0"
+    />
+  </section>
+  <div class="counter">
+    <button type="button" @click="count++" v-if="count >= -1">+</button>
+    <button type="button" @click="count--" v-if="count > 0">-</button>
+
+    <h2>{{ count }}</h2>
+  </div>
+</template>
